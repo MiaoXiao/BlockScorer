fb9cef649078c92e33664c1e8ed9b9f92ceb1a1b
Rica Feng <rfeng001@ucr.edu>
2016-12-26 02:15:49 -0800

    Picking up and releasing objects now tracked correctly

--- Assets/Scripts/CratePickUp.cs
+++ Assets/Scripts/CratePickUp.cs
@@ -11,7 +11,28 @@ public class CratePickUp : MonoBehaviour, IInteractable
     public Color normalColor = Color.white;
     public Color activeColor = Color.green;
 
+    //References
+    private Rigidbody CrateRB;
+    private GameObject Player;
+    private Camera MainCamera;
+
+    /// <summary>
+    /// Distance crate is carried in front of player
+    /// </summary>
+    public float holdDistance = 1f;
+
+    /// <summary>
+    /// Set to true when crate is picked up
+    /// </summary>
     private bool isGrabbed = false;
+
+    private void Awake()
+    {
+        CrateRB = GetComponent<Rigidbody>();
+        Player = GameObject.FindGameObjectWithTag("Player");
+        MainCamera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+
+    }
 
     public string OnActivate()
     {
@@ -29,20 +50,21 @@ public class CratePickUp : MonoBehaviour, IInteractable
 
     public void OnInteract(GameObject actor)
     {
-        if (isGrabbed)
+        isGrabbed = !isGrabbed;
+        if (!isGrabbed)
             ReleaseObject(actor);
         else
-            GrabObject(actor);
-
-        isGrabbed = !isGrabbed;
+            CrateRB.useGravity = false;
     }
 
-    /// <summary>
-    /// Grab the object and place in front of player
-    /// </summary>
-    public void GrabObject(GameObject actor)
+    private void FixedUpdate()
     {
-
+        if (isGrabbed)
+        {
+            Debug.Log("grabbing");
+            CrateRB.MovePosition(MainCamera.transform.position + MainCamera.transform.forward * holdDistance * Time.fixedDeltaTime);
+            //Debug.Log(MainCamera.transform.position + " " + MainCamera.transform.forward + " " + holdDistance);
+        }
     }
 
     /// <summary>
@@ -50,6 +72,7 @@ public class CratePickUp : MonoBehaviour, IInteractable
     /// </summary>
     public void ReleaseObject(GameObject actor)
     {
-
+        Debug.Log("release object");
+        CrateRB.useGravity = true;
     }
 }
--- Assets/Scripts/PlayerInteractController.cs
+++ Assets/Scripts/PlayerInteractController.cs
@@ -6,7 +6,20 @@ public class PlayerInteractController : MonoBehaviour
 	public PlayerInput input;
 	public UIController uiController;
 	private IInteractable currentInteractionTarget { get; set; }
-	
+
+    /// <summary>
+    /// Set to true when looking at an interactable object
+    /// </summary>
+    private bool mouseOvered = false;
+    private Camera MC;
+    private GameController GC;
+
+    private void Awake()
+    {
+        MC = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+        GC = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
+    }
+
 	private void Start()
 	{
 		if(input != null)
@@ -14,7 +27,32 @@ public class PlayerInteractController : MonoBehaviour
 			input.onActivate += OnActivate;
 		}
 	}
-	
+
+    private void Update()
+    {
+        if (GC.GamePaused)
+            return;
+
+        RaycastHit hit;
+        Ray ray = MC.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            IInteractable interactComponent = hit.transform.GetComponent<IInteractable>();
+            if (interactComponent != null)
+            {
+                ActivateInteraction();
+                mouseOvered = true;
+            }
+            else
+            {
+                DeactivateInteraction();
+                mouseOvered = false;
+            }
+        }
+        Debug.Log(mouseOvered);
+    }
+
 	private void OnDestroy()
 	{
 		if(input != null)
@@ -22,29 +60,31 @@ public class PlayerInteractController : MonoBehaviour
 			input.onActivate += OnActivate;
 		}
 	}
-	
+
 	private bool OnActivate()
 	{
 		if(currentInteractionTarget == null)
 			return false;
 		
 		currentInteractionTarget.OnInteract(this.gameObject);
-		DeactivateInteraction();
 		
 		return true;
 	}
-	
-	private void OnTriggerEnter(Collider other)
-	{
-		IInteractable interactComponent = other.GetComponent<IInteractable>();
-		if(interactComponent == null)
-			return;
-		
-		if(currentInteractionTarget != null)
-			currentInteractionTarget.OnDeactivate();
-		
-		currentInteractionTarget = interactComponent;
-		ActivateInteraction();
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (!mouseOvered)
+            return;
+
+        IInteractable interactComponent = other.GetComponent<IInteractable>();
+        if (interactComponent == null)
+            return;
+
+        if (currentInteractionTarget != null)
+            currentInteractionTarget.OnDeactivate();
+
+        currentInteractionTarget = interactComponent;
+        
 	}
 	
 	private void OnTriggerExit(Collider other)
@@ -65,6 +105,9 @@ public class PlayerInteractController : MonoBehaviour
 	
 	private void ActivateInteraction()
 	{
+        if (currentInteractionTarget == null)
+            return;
+
 		string pickupName = currentInteractionTarget.OnActivate();
 		if(uiController != null)
 			uiController.ActivateTargetName(pickupName);
