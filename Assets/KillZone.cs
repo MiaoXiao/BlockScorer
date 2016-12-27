55eecaa79fc0d9bfde9d1556ca4628dde4152f29
Rica Feng <rfeng001@ucr.edu>
2016-12-26 22:00:13 -0800

    Added kill zone if player falls off

---
+++ Assets/KillZone.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillZone : MonoBehaviour
+{
+    private GameController GC;
+    private GameObject Player;
+    private UIController UC;
+
+    private void Awake()
+    {
+        GC = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
+        Player = GameObject.FindGameObjectWithTag("Player");
+        UC = GameObject.FindGameObjectWithTag("GUI").GetComponent<UIController>();
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+            GC.EndGame();
+        else
+        {
+            //Depool the dropped object and give time penalty
+        }
+
+    }
+}
