bb55ceef6760d325c555d431a33bad3c9a7d58e3
Rica Feng <rfeng001@ucr.edu>
2016-12-29 10:15:00 -0800

    Rules now displayed at the start of the game

---
+++ Assets/Scripts/DeactivateObject.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeactivateObject : MonoBehaviour
+{
+    [SerializeField]
+    private float DeactivateInSeconds = 10f;
+
+	// Use this for initialization
+	void Start ()
+    {
+        Invoke("Deactivate", DeactivateInSeconds);	
+	}
+	
+    void Deactivate()
+    {
+        gameObject.SetActive(false);
+    }
+}
